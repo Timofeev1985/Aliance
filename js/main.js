195637f7d7e6f59d03ad855978d3f3d6c5c255ed
Timofeev1985 <timon_1985_@mail.ru>
2025-01-12 18:26:06 +0300

    fix: mobile menu js

--- js/main.js
+++ js/main.js
@@ -2,16 +2,31 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+const lightModeOn = () => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/icons.svg#logo-black";
+};
+const lightModeOff = () => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/icons.svg#logo";
+};
+const openMenu = (event) => {
+  menu.classList.add("is-open");
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden";
+  lightModeOn();
+};
+const closeMenu = (event) => {
+  menu.classList.remove("is-open");
+  mMenuToggle.classList.remove("close-menu");
+
+  document.body.style.overflow = "";
+  lightModeOff();
+};
 window.addEventListener("scroll", () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/icons.svg#logo-black";
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/icons.svg#logo";
-  }
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle("is-open");
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
